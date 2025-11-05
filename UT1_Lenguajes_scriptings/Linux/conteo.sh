1a363634760af514de039292b1930f1a03b5f1d4
Alicia Sáinz Pérez <ASR209@iesmhp.local>
2025-11-05 11:11:03 +0100

    Creado archivo conteo.sh

---
+++ UT1_Lenguajes_scriptings/Linux/conteo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#=======================
+#Nombre:conteo.sh
+#Descripcion:Muestra la cantidad de archivos de un directorio
+#Autor:Alicia Sainz 
+#Fecha:05/11/2025
+#Comentario:
+#=======================
+if [ -z "$1" ]; then
+    echo "Uso: $0 /ruta/al/directorio"
+    exit 1
+fi
+DIRECTORIO="$1"
+
+CONTEO=$(find "$DIRECTORIO" -type f | wc -l)
+echo "📂 Número de archivos en '$DIRECTORIO': $CONTEO"
